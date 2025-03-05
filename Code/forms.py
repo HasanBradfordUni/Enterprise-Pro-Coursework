32d5fd7bc49f7c40b5277e21b889c6e4ec66999b
hxmm3r <haziz8@bradford.ac.uk>
2025-03-05 17:37:53 +0000

    Adding rest of the forms

--- Code/forms.py
+++ Code/forms.py
@@ -6,3 +6,39 @@ class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = StringField('Password', validators=[DataRequired()], render_kw={"type": "password"})
     submit = SubmitField('Submit')
+
+class CreateUserForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()], render_kw={"type": "password"})
+    role = StringField('Role', validators=[DataRequired()])
+    team = StringField('Team', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class UpdateUserDetailsForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    password = StringField('Password', validators=[DataRequired()], render_kw={"type": "password"})
+    role = StringField('Role', validators=[DataRequired()])
+    team = StringField('Team', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class CreateProjectForm(FlaskForm):
+    project_title = StringField('Project Title', validators=[DataRequired()])
+    project_details = StringField('Project Details', validators=[DataRequired()])
+    project_status = StringField('Project Status', validators=[DataRequired()])
+    project_review = StringField('Project Review', validators=[DataRequired()])
+    project_owner = StringField('Project Owner', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
+class UpdateProgressForm(FlaskForm):
+    progress_update = StringField('Progress Update', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class EditTaskForm(FlaskForm):
+    task_id = StringField('Task ID', validators=[DataRequired()])
+    task_title = StringField('Task Title', validators=[DataRequired()])
+    task_details = StringField('Task Details', validators=[DataRequired()])
+    task_status = StringField('Task Status', validators=[DataRequired()])
+    task_assigned_date = StringField('Task Assigned Date', validators=[DataRequired()])
+    task_due_date= StringField('Task Due Date', validators=[DataRequired()])
+    submit = SubmitField('Submit')
